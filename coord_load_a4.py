5ed6884018eb3a5e06f3a3b707622797eec3f5a4
max-over <55400375+max-over@users.noreply.github.com>
2024-07-28 23:39:28 +0300

    Add files via upload

---
+++ coord_load_a4.py
@@ -0,0 +1,22 @@
+import random
+random.seed(10)
+from sb3_contrib import RecurrentPPO
+from stable_baselines3.common.evaluation import evaluate_policy
+from BG_RL_a4_obs8_env import BeerGameEnv
+
+vec_env = BeerGameEnv()
+
+models_path = "E:/SC_Beer_Game_Python-main/SC_Beer_Game_Python-main/models/1721960641/900000.zip"
+model = RecurrentPPO.load(models_path, env=vec_env)
+
+vec_env = model.get_env()
+obs = vec_env.reset()
+
+mean_reward, std_reward = evaluate_policy(model, vec_env, n_eval_episodes=3)
+print(f"mean_reward:{mean_reward:.2f} +/- {std_reward:.2f}")
+
+
+
+
+
+
